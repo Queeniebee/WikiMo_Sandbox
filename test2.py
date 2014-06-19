b39946011dc6a1f13ab9dccff7f7b159b966c7c7
mozilla <mozilla@jfleuranth-07711.local>
2014-06-19 10:02:18 -0700

    figured out how to get listing of pages within Special namespace

--- test2.py
+++ test2.py
@@ -3,7 +3,20 @@ from pprint import pprint
 
 
 wikiMo = mwclient.Site(('https', 'wiki.mozilla.org'), path='/' )
-pages = wikiMo.Pages['Category:Special']
+# pages = wikiMo.Pages['Special:SpecialPages']
 
+"""
 for page in pages:
     pprint(pages.name)
+"""
+# pprint(wikiMo.namespaces)
+
+
+special = wikiMo.api('query', meta='siteinfo', siprop='specialpagealiases')
+pprint(special)
+
+"""
+nsindexes = dict((v,k) for k,v in wikiMo.namespaces.iteritems())
+for page in wikiMo.allpages(namespace=nsindexes['Template']):
+    print page.name
+"""
